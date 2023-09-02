6e0dae3d1e8b15d5264e6eef109b0b9740c367f6
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-02 22:34:36 +0530

    Count frequency in array

---
+++ Demoofinput.js
@@ -0,0 +1,4 @@
+const prompt = require("prompt-sync")()
+ let c
+ c = prompt("Enter the User Name")
+ console.log("The User Name", c);
---
+++ frequencyofarray.js
@@ -0,0 +1,11 @@
+let arr = [1,2,1,2,3,4,3,4,5]
+let count = {};
+arr.forEach((val,index)=>{
+          if (!count[val]) {
+                    count[val] = 1;
+          }
+          else{
+                    count[val] = count[val]+1;
+          }
+})
+console.log(count);
