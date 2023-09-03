7f9759d4d9a5178666894d1be98baa28b57cf0e7
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-04 02:36:13 +0530

    Average of array

--- averageofarray.js
+++ averageofarray.js
@@ -1,8 +1,10 @@
 const prompt = require("prompt-sync")()
-let arr = [2,2,2,2,2]
+let arr = [2,3,4,5,6,7,8,9]
 let temp =0;
 for(  let j=0; j<arr.length ;j++){
  temp = temp + arr[j];
 }
 
-console.log(temp/arr.length);
+
+console.log("The sum Of Array", temp);
+console.log(" The Average of the Array", temp/arr.length);
