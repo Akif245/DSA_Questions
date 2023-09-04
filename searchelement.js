e2482971ec66e0e58b9bc5527148c4b59f81ae97
Mohammed Abdul Akif Ahmed <abdulakif44570@gmail.com>
2023-09-05 00:32:31 +0530

    search element in array

---
+++ searchelement.js
@@ -0,0 +1,13 @@
+ let b= [1,2,3,4,5]
+ let c = 5
+ console.log(b.indexOf(c));
+                    //or
+ let arr = [1, 2, 3, 4, 5, 6, 7];
+ let a = 2;
+ let index = undefined;
+ for (let i = 0; i < arr.length; i++) {
+  if (arr[i] == a) {
+    index = i;
+  }
+}
+ console.log(index);
